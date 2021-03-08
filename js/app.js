3eab8457560bb4ac8d2c3dca840b5c6095c13c2b
NarminGale <narmingale@gmail.com>
2021-03-08 12:03:46 +0400

    Provide functionality of smooth scroll

--- js/app.js
+++ js/app.js
@@ -10,3 +10,24 @@ const links = document.getElementById('nav-links')
 navBtn.addEventListener('click', () => {
   links.classList.toggle('show-links')
 })
+
+// smooth scrool
+const scrollLinks = document.querySelectorAll('.scroll-link')
+
+scrollLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault()
+    links.classList.remove('show-links')
+
+    const id = e.target.getAttribute('href').slice(1)
+    const element = document.getElementById(id)
+
+    let position = element.offsetTop - 62
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+      behavior: 'smooth',
+    })
+  })
+})
